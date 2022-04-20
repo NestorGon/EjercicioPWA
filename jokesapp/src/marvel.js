4be11337351f3c5995073ed4d2a3d6ff44a89d0a
Nestor Gonzalez <nf.gonzalez@uniandes.edu.co>
2022-04-20 11:14:07 -0500

    Correccion LocalStorage

--- jokesapp/src/marvel.js
+++ jokesapp/src/marvel.js
@@ -7,22 +7,18 @@ function Marvel() {
 
     useEffect(()=>{
         if(!navigator.onLine){
-            console.log('IS OFFLINE')
             if(localStorage.getItem("characters") === null) {
-                console.log('No characters found');
                 setCharacters("Loading...")
             } else {
-                console.log('Characters found: ',localStorage.getItem("characters"));
-                setCharacters(localStorage.getItem("characters"));
+                setCharacters(JSON.parse(localStorage.getItem("characters")));
             }
         } else {
             fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=7d65f7d421178128d498070af0170141&hash=d656e8ee197b36219f09752b48113180')
             .then(resp=>resp.json())
             .then(resp=>resp.data.results)
             .then(characters=>{
-                console.log(characters);
                 setCharacters(characters);
-                localStorage.setItem("characters", characters);
+                localStorage.setItem("characters", JSON.stringify(characters));
             });
         }
     }, []);
